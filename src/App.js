9ea4a4851444bfbc7dd76bdf2d6926af10b89b81
chenrong1@linkstec.com <chenrong1@linkstec.com>
2021-08-19 14:58:15 +0800

    007

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
-import alert from 'alert';
 
 class App extends Component {
   render() {
@@ -25,42 +24,10 @@ class App extends Component {
           >
             Learn React
           </a>
-
-          <a
-            className="App-link"
-            href="../pages/test"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            test.jsへ
-          </a>
         </header>
-        <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-          <p>Test</p>
-          <div>
-            <textarea id="signed" rows="12" cols="120"></textarea>
-          </div>
-          <button id="callLambda" onClick={myClick}>Lambda呼び出す1</button>
-      </main>
       </div>
     );
   }
 }
-function myClick(){
 
-  alert("onclick");
-  var request = require('request');
-  request('https://yhrdyh4j8h.execute-api.us-east-2.amazonaws.com/chenr-test001/chenr-function', function (err, response, body) {
-    if (!err && response.statusCode == 200) {
-      document.getElementById('signed').value= response.body;
-    } else {
-      alert("API呼出エラー");
-    }
-  })
-
-}
 export default withAuthenticator(App);
